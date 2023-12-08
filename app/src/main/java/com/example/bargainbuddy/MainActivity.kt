895b10c54ea8a82d04be54c30e97978d7561324a
tylerboy0604 <74601919+tylerboy0604@users.noreply.github.com>
2023-12-08 11:22:40 +0100

    added button to login

--- app/src/main/java/com/example/bargainbuddy/MainActivity.kt
+++ app/src/main/java/com/example/bargainbuddy/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.bargainbuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnRegister: Button = findViewById(R.id.gotologin);
+
+        btnRegister.setOnClickListener() {
+            goToLogin();
+        }
+
+    }
+
+    fun goToLogin(){
+        val intent = Intent(this, login::class.java)
+        startActivity(intent)
     }
 }
